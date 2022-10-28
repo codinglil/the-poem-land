d3fe6161d46d26c50e05d5aab7213987207ca74a
codinglil <102619991+codinglil@users.noreply.github.com>
2022-10-27 23:27:51 -0400

    added a video and js on form

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let form = document.getElementById("formId");
 
+function sendMessage() {
+  let name = document.getElementById("name").value;
+  console.log(name);
+  alert(
+    `Thank you for contacting me ${name.toUpperCase()}! I will reply within 48 hours.`
+  );
+}
+form.addEventListener("submit", sendMessage);
